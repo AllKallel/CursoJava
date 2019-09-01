542b19e4b208de233fa4efe090cb2c3b3ca32b45
allan <allan_xd17@hotmail.com>
2019-09-01 11:55:19 -0300

    commit inicial

---
+++ src/OlaMundo.java
@@ -0,0 +1,6 @@
+//palavras com cores diferentes são palavras reservadas do java
+public class OlaMundo {
+    public static void main(String[] args){
+        System.out.println("olá mundo!");
+    }
+}
