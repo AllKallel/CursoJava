89009fcfd5453ce4e4cad7803d1fe485eb2112b0
allan <allan_xd17@hotmail.com>
2019-09-01 17:14:23 -0300

    operadores mais

---
+++ src/OperadoresDeAtribuicao.java
@@ -0,0 +1,11 @@
+public class OperadoresDeAtribuicao {
+    public static void main(String[] args) {
+
+        // = , -= , += , *= , /= , %=
+        // são operadores de atribuição
+        int numero = 11;
+        numero %= 2;
+
+        System.out.println(numero);
+    }
+}
---
+++ src/OperadoresLogicos.java
@@ -0,0 +1,12 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        //  && AND
+        //  ||  OR
+
+        int idade = 18;
+        float salario = 1000;
+            System.out.println(idade==18 && salario > 999);
+
+    }
+
+}
