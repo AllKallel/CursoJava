a5683877bcd64d9dcdf370d15315da3c99ce3211
allan <allan_xd17@hotmail.com>
2019-10-14 11:22:48 -0300

    controle de fluxo 4 - while/do while e for

---
+++ src/controleDeFluxo4.java
@@ -0,0 +1,21 @@
+public class controleDeFluxo4 {
+    public static void main(String[] args) {
+        int contador = 11;
+        //O bloco de codigo é execuado enquanto a condição for verdadeira.
+        while(contador<10){
+            System.out.println(++contador);
+        }
+
+        //o bloco de codigo é executado ao menos uma vez.
+        do{
+            System.out.println("Estou no do while");
+        }while(contador<10);
+
+        //no for é declarado uma variável, uma condição e o que acontece com a variável de acordo com a exec. do
+        //codigo. O bloco será executado enquanto a condição for verdadeira.
+        for(int i =0; i<10 ; i++){
+            System.out.println("valor de contador 1 =" +i);
+        }
+    }
+
+}
