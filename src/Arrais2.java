8b9570e91b3820fa90242cabbe793769b2dd0e24
allan <allan_xd17@hotmail.com>
2019-10-15 15:37:48 -0300

    array 2

--- src/Arrais2.java
+++ src/Arrais2.java
@@ -1,6 +1,18 @@
 public class Arrais2 {
     public static void main(String[] args) {
-        int[] idade = new int[3];
-        teste de erro
+        //byte, short, int, long, float double = 0;
+        //char = \u0000 representação de espaço em branco
+        //boolean = false
+        //references = null
+        String[] nome = new String[3];
+
+            nome[0] = "kallel";
+            nome[1] = "sara";
+            nome[2] = "allan";
+
+            for (int i = 0; i<nome.length;i++){
+                //length mostra o tamanho do array
+                System.out.println(nome[i]);
+            }
     }
 }
