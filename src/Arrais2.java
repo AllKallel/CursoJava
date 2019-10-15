76d1f80544d5d214b31e18a3ba5fed20bf6f174f
allan <allan_xd17@hotmail.com>
2019-10-15 10:29:09 -0300

    cntrole de fluxo - Arrays1

---
+++ src/Arrais2.java
@@ -0,0 +1,5 @@
+public class Arrais2 {
+    public static void main(String[] args) {
+        int[] idade = new int[3];
+    }
+}
---
+++ src/Arrays1.java
@@ -0,0 +1,12 @@
+public class Arrays1 {
+    public static void main(String[] args) {
+        /*
+        * a variável idade é do tipo references, que faz referência a um objeto do tipo int de 4 posições na memória
+        * */
+        int[] idade = new int[3];
+        System.out.println(idade[0]);
+        System.out.println(idade[1]);
+        System.out.println(idade[2]);
+        System.out.println(idade[3]);
+    }
+}
