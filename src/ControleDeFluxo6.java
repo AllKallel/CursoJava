6ae556e7b11d48e7d0bbdbac1e7f75e22dc1d0bc
allan <allan_xd17@hotmail.com>
2019-10-15 09:59:33 -0300

    cntrole de fluxo - continue e break

--- src/ControleDeFluxo6.java
+++ src/ControleDeFluxo6.java
@@ -3,6 +3,13 @@ public class ControleDeFluxo6 {
         //Dado o valor de um carro descubra em quantas vezes ele pode ser parcelado.
         //O valor da parcela não pode ser menor que 1000.
         double valorCarro = 30000;
-
+        for(int parcela = (int) valorCarro; parcela>=1; parcela--){
+            double valorParcela = valorCarro / parcela;
+            if(valorParcela<1000){
+                //o continue ignora o que vem após ele e pula para a próxima iteração
+                continue;
+            }
+            System.out.println(parcela+" parcelas R$"+valorParcela);
+        }
     }
 }
