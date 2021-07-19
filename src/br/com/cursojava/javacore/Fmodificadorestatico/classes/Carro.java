4dcebc232c2e4c76fee0f633bb34aa9f8e35f721
Allan Ramos <allan_xd17@hotmail.com>
2021-07-19 17:59:18 -0300

    Atributos estáticos

--- src/br/com/cursojava/javacore/Fmodificadorestatico/classes/Carro.java
+++ src/br/com/cursojava/javacore/Fmodificadorestatico/classes/Carro.java
@@ -1,4 +1,41 @@
 package br.com.cursojava.javacore.Fmodificadorestatico.classes;
 
 public class Carro {
+    private String nome;
+    private double velocidadeMaxima;
+    public static double getVelocidadeLimite = 240;
+
+    //METODO CONSTRUTOR DA CLASSE_________________________________________________________________________________
+    public Carro(String nome, double velocidadeMaxima) {
+        this.nome = nome;
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    public Carro() {
+    }
+
+    //GETTERS AND SETTERS_________________________________________________________________________________________
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getVelocidadeMaxima() {
+        return velocidadeMaxima;
+    }
+
+    public void setVelocidadeMaxima(double velocidadeMaxima) {
+        this.velocidadeMaxima = velocidadeMaxima;
+    }
+
+    //METODO IMPRIMIR___________________________________________________________________________________________
+    public void imprimir() {
+        System.out.println("_______________________________________________");
+        System.out.println("Nome = " + this.nome);
+        System.out.println("Velocidade máxima = " + this.velocidadeMaxima);
+        System.out.println("Velocidade limite= " + getVelocidadeLimite);
+    }
 }
--- src/br/com/cursojava/javacore/Fmodificadorestatico/test/CarroTest.java
+++ src/br/com/cursojava/javacore/Fmodificadorestatico/test/CarroTest.java
@@ -1,7 +1,24 @@
 package br.com.cursojava.javacore.Fmodificadorestatico.test;
 
+import br.com.cursojava.javacore.Fmodificadorestatico.classes.Carro;
+
 public class CarroTest {
     public static void main(String[] args) {
+        Carro a = new Carro("Celta", 120);
+        Carro b = new Carro("Mercedes", 240);
+        Carro c = new Carro("Tesla", 300);
+
+        a.imprimir();
+        b.imprimir();
+        c.imprimir();
+
+        Carro.getVelocidadeLimite = 220;
+
+        a.imprimir();
+        b.imprimir();
+        c.imprimir();
+
+
 
     }
 }
